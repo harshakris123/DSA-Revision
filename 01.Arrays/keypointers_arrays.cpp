2e8069600c84c3b73678b0d856f193dcd87b7576
Reddy Harsha Vardhan <harsha.kris3008@gmail.com>
2026-05-12 17:25:56 +0530

    donne with arrays hard

--- 01.Arrays/keypointers_arrays.cpp
+++ 01.Arrays/keypointers_arrays.cpp
@@ -1,1 +1,5 @@
-question number 10,11 in hard are to be done
+// 
+// question number 10,11 in hard are to be done, involves merge sort, so will be done at the end.
+/* also check out sorting algorithms.
+
+// Sliding Window works when: Array contains only positive numbers (or non-negative).
