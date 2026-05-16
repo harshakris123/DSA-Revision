5cf21b499f9695c31cc70ee09435880ae6899180
Reddy Harsha Vardhan <harsha.kris3008@gmail.com>
2026-05-16 19:34:22 +0530

    modified arr

--- 02.Binary Search/1D Arrays/10.Search_in_rotated_sorted_array_with_duplicates.cpp
+++ 02.Binary Search/1D Arrays/10.Search_in_rotated_sorted_array_with_duplicates.cpp
@@ -59,6 +59,28 @@ bool search(vector<int>& nums, int target) {
     }
     return false;
 }
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low < high)
+        {
+            int mid = low + (high- low)/2;
+            if(nums[mid] < nums[high])
+            {
+                high = mid;
+            }else if(nums[mid] > nums[high])
+            {
+                low = mid+1;
+            }else{
+                high--;
+            }
+        }
+        return nums[low];
+    }
+};
 
 // TIME COMPLEXITY: O(log n)
 // SPACE COMPLEXITY: O(1)
